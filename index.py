1abbc793f7cdaae4cd9b6730e1c2ea9a253e73a2
JuanCa <jucavaflo.16@gmail.com>
2021-10-19 17:03:58 -0500

    Upload

---
+++ index.py
@@ -0,0 +1,24 @@
+
+class Cuenta:
+
+     def __init__(self,numero_cuenta,saldo,interes_anual):
+         self.numero_cueta = numero_cuenta
+         self.saldo = saldo
+         self.interes_anual = interes_anual
+
+     
+
+   
+def main():
+    ss = Cuenta(1250,12000,2)
+    print(ss.numero_cueta)
+
+    
+    
+
+
+
+if __name__ == "__main__":
+    main()
+
+         
